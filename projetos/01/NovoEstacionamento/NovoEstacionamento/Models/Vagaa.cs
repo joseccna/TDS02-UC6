f00149fe8ab79e90618ab5bc5058c4005998f424
jose.ccna <jose.ccna@gmail.com>
2025-10-06 22:08:00 -0300

    Novo projeto de estacionamento

---
+++ projetos/01/NovoEstacionamento/NovoEstacionamento/Models/Vagaa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoEstacionamento.Models
+{
+    internal class Vagaa
+    {
+        public int Id { get; set; }
+        public string Localizacao { get; set; }
+        public string Tipo { get; set; }
+
+    }
+
+
+
+
+}
---
+++ projetos/01/NovoEstacionamento/NovoEstacionamento/Program.cs
@@ -0,0 +1,6 @@
+﻿using NovoEstacionamento.Models;
+
+
+var c1 = new Cliente("João Silva");
+Console.WriteLine(c1.Nome);
+
