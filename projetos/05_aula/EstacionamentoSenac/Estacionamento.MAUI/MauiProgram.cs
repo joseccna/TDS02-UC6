9625152069e2691adfdab847164aa2def356c0a6
jose.ccna <jose.ccna@gmail.com>
2025-11-06 22:00:36 -0300

    Efetuado o desafio aula-05

---
+++ projetos/05_aula/EstacionamentoSenac/Estacionamento.MAUI/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Estacionamento.MAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddSingleton<HttpClient>();// tem que configura essa linha 
+            //pois o projeto esta para descktop e essa linha faz requisição HTTP.
+
+            return builder.Build();
+        }
+    }
+}
---
+++ projetos/05_aula/EstacionamentoSenac/Estacionamento.MAUI/Models/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.MAUI.Models
+{
+    internal class Veiculo
+    {
+        public int Id { get; set; }
+        public string? Placa { get; set; }
+        public string? Modelo { get; set; }
+        public string? Cor { get; set; }
+        public int? MotoristaId { get; set; }
+        public Motorista? motorista { get; set; }
+    }
+}
