1f5905b8fcabf064920bc6d76e843425644638a5
jose.ccna <jose.ccna@gmail.com>
2025-11-11 22:01:12 -0300

    projeto com API

---
+++ projetos/05_aula/EstacionamentoSenac/Estacionamento.MAUI/Models/Vagas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamento.MAUI.Models
+{
+    public class Vagas
+    {
+        public int Id { get; set; }
+        public string? Localizacao { get; set; }
+        public string? TipoVaga { get; set; }
+    }
+}
